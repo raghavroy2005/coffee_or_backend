07158aeaeef7e775b6144d5ee0266b99f2985c36
raghavroy2005 <raghav2005kumar.gmail.com>
2025-10-23 00:54:07 +0530

    add user and video model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -51,14 +51,15 @@ const userSchema = new Schema({
 userSchema.pre("save", async function (next) {
     if(!this.isModified("password")) return next();
 
-    this.password = bcrypt.hash(this.password, 10)
+    this.password = await bcrypt.hash(this.password, 10)
     next()
 })
-
+ 
 userSchema.methods.isPasswordCorrect = async function 
 (password){
     return await bcrypt.compare(password,this.password)
 }
+
 
 userSchema.methods.generateAccessToken = function(){
     return jwt.sign(
@@ -74,10 +75,11 @@ userSchema.methods.generateAccessToken = function(){
         }
     )
 }
+
 userSchema.methods.generateRefreshToken = function(){
     return jwt.sign(
         {
-            _id: this._id,
+            _id: this._id, 
            
         },
         
@@ -87,6 +89,5 @@ userSchema.methods.generateRefreshToken = function(){
         }
     )
 }
-
 
 export const User = mongoose.model("User",userSchema)
